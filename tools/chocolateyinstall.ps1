c1ee9ba8e14dc1d7c0eaab93b59b1f0330301cf2
Vladimir Budylnikov <me@nett00n.org>
2022-03-29 00:17:24 +0300

    update

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,6 +1,6 @@
 $packageName = 'mkv-muxing-batch-gui_chocolatey'
-$url = 'https://github.com//yaser01/mkv-muxing-batch-gui/releases/download/2.1/MKV.Muxing.Batch.GUI.x32.v2.1.Portable.zip'
-$checksum = '4c5e37f11d97beb0013c70add74eae6fdb84f4570384579210a7243ae6b54422'
+$url = 'https://github.com/https://github.com/yaser01/mkv-muxing-batch-gui/releases/download/2.1/MKV.Muxing.Batch.GUI.x64.v2.1.Portable.zip'
+$checksum = '0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5'
 $checksumType = 'sha256'
 $unzipLocation = $(Split-Path -parent $MyInvocation.MyCommand.Definition)
 
